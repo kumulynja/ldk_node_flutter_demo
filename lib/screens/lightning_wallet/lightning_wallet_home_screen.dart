2cc8a1d4cfe0160785e369ece4bf9aa88589e289
kumulynja <b@kumuly.dev>
2023-06-18 00:28:21 +0200

    add expansion panel list for all lightning operations

--- lib/screens/lightning_wallet/lightning_wallet_home_screen.dart
+++ lib/screens/lightning_wallet/lightning_wallet_home_screen.dart
@@ -8,8 +8,33 @@ import 'package:ldk_node_flutter_demo/widgets/wallet_info_container.dart';
 import 'package:ldk_node_flutter_demo/widgets/lightning_funding_actions.dart';
 import 'package:ldk_node_flutter_demo/widgets/lightning_payment_actions.dart';
 
-class LightningWalletHomeScreen extends StatelessWidget {
-  const LightningWalletHomeScreen({super.key});
+class LightningWalletHomeScreen extends StatefulWidget {
+  const LightningWalletHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  LightningWalletHomeScreenState createState() =>
+      LightningWalletHomeScreenState();
+}
+
+class LightningWalletHomeScreenState extends State<LightningWalletHomeScreen> {
+  final List<bool> _isOpen = [
+    false,
+    false,
+    true
+  ]; // Start with Transaction History open
+
+  void _togglePanel(int index) {
+    // Make sure only one panel is open at a time
+    setState(() {
+      for (int i = 0; i < _isOpen.length; i++) {
+        if (i != index) {
+          _isOpen[i] = false;
+        } else {
+          _isOpen[i] = !_isOpen[i];
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +60,53 @@ class LightningWalletHomeScreen extends StatelessWidget {
               network:
                   state is LightningNodeRunSuccess ? state.network.name : null,
             ),
-            const LightningFundingActions(),
-            const LightningPaymentActions(),
-            const TransactionHistory(),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsetsDirectional.only(top: 0),
+                children: [
+                  ExpansionPanelList(
+                    expansionCallback: (int index, bool isExpanded) {
+                      _togglePanel(index);
+                    },
+                    children: [
+                      ExpansionPanel(
+                        backgroundColor:
+                            Theme.of(context).colorScheme.background,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return const Text('Funding Actions');
+                        },
+                        body:
+                            const LightningFundingActions(), // Custom Widget for Funding Actions
+                        isExpanded: _isOpen[0],
+                        canTapOnHeader: true,
+                      ),
+                      ExpansionPanel(
+                        backgroundColor:
+                            Theme.of(context).colorScheme.background,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return const Text('Payment Actions');
+                        },
+                        body:
+                            const LightningPaymentActions(), // Custom Widget for Payment Actions
+                        isExpanded: _isOpen[1],
+                        canTapOnHeader: true,
+                      ),
+                      ExpansionPanel(
+                        backgroundColor:
+                            Theme.of(context).colorScheme.background,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return const Text('Transaction History');
+                        },
+                        body:
+                            const TransactionHistory(), // Custom Widget for Transaction History
+                        isExpanded: _isOpen[2],
+                        canTapOnHeader: true,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
